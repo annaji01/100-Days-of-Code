5d343c47b21480cb9d8b2a92e298f7d04121ac7d
Digpal Singh Rathore <75355851+dsrathore1@users.noreply.github.com>
2022-06-05 09:34:37 +0000

    Linked initialized

---
+++ JAVA/LinkedList.java
@@ -0,0 +1,13 @@
+class LinkedList{
+  Node head; // Head of node
+  
+  class Node{
+    int data; // Data
+    Node next; // Pointer
+
+    // Create a Constructor
+    Node(int d){
+      data d;
+    }
+  }
+}
